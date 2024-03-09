e54cc4df5d936b7e5adb600aff45b0714eca8f57
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2024-03-09 14:58:45 +0530

    feat: added routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
 import "./App.css";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Username from "./components/Username";
+import Register from "./components/Register";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element:<div>Root Router</div>
+    element:<Username/>
   },
   {
     path: '/register',
-    element:<div>Register Route</div>
+    element:<Register/>
+  },
+  {
+    path: '/password',
+    element:<Password/>
+  },
+  {
+    path: '/profile',
+    element: <Profile/>
+  },
+  {
+    path: '/recovery',
+    element: <Recovery/>
+  },
+  {
+    path: '/reset',
+    element: <Reset/>
+  },
+  {
+    path: '/*',
+    element: <PageNotFound/>
   }
+
 ])
 
 function App() {
